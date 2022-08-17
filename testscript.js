d14794e306e3919a0a04b5e4fa543bd54baa26a9
LukaszDM0 <104510586+LukaszDM0@users.noreply.github.com>
2022-08-17 14:53:05 +0200

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -1,7 +1,6 @@
-require(["dojo/dom", "dojo/request", "dojo/json",],
-    function(dom, request, JSON){
+require(["dojo/dom", "dojo/request"],
+    function(dom, request){
         var resultDiv = dom.byId("resultDiv");
-        var resultPic = dom.byId("resultPic");
 
         request.get("https://jsonplaceholder.typicode.com/photos/6", {
             handleAs: "json"
@@ -11,16 +10,42 @@ require(["dojo/dom", "dojo/request", "dojo/json",],
             "<p class='Title'>Title</p>" + "<p class='Data'>" + data.title + "</p>"
 
             // Display image
-            var pic = "<img src=" + data.url + ">"
+            //var pic = "<img src=" + data.url + ">"
 
-            resultDiv.innerHTML = info;
-            resultPic.innerHTML = pic;
+            var old = resultDiv.innerHTML;
+            resultDiv.innerHTML = info + old;
 
         },
         function(error){
             // Display the error returned
-            result.innerHTML = error;
+            resultDiv.innerHTML = error;
+        });
+    },
+);
+
+require(["dijit/form/Button", "dojo/dom", "dojo/request", "dojo/domReady!"], function(Button, dom, request) {
+    var button = new Button({
+        label: "Change picture",
+        onClick: function(){ 
+            
+            
+            var changePic = dom.byId("changePic");
+
+        request.get("https://jsonplaceholder.typicode.com/photos/12", {
+            handleAs: "json"
+        }).then(function(data){
+            
+            // Change image
+            var pic = data.url;
+            changePic.src = pic
+
+        },
+        function(error){
+            // Display the error returned
+            changePic.innerHTML = error;
         });
     }
-    
-);
+    }, "btn");
+
+    button.startup();
+});
